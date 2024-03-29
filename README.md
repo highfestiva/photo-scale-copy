1f566e5a30c5e9fbe61191cc56669e970b6e4320
Jonte <highfestiva@gmail.com>
2024-03-29 11:58:21 +0100

    Allow for improved wildcards when picking out folders

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 Downsize and rename pictures for old-school photo frame. Run like this:
 
 ```bash
-cd /f/
-python3 scale_images.py /c/Users/SomeUser/Pictures img 320 234
+python3 scale_images.py 'c:/Users/SomeUser/Pictures/Year 202[2-4]' /d/img/ 320 234
 ```
 
-Where 320x234 is the pixel resolution of of they crappy photo frame.
+Where 320x234 is the pixel resolution of some crappy photo frame.
--- scale_images.py
+++ scale_images.py
@@ -1,103 +1,82 @@
 #!/usr/bin/env python3
 
 
-import PIL.Image
+from glob import glob
+from PIL import Image, ImageOps
 import os
 import shutil
 import sys
+from tqdm import tqdm
 
 
-img_extensions = set([".JPG", ".jpg"])
-imgcnt = 0
-w = 320
-h = 240
-imgindex = 0
+img_extensions = '.JPG .jpg'.split()
+bad_paths = '\\. /. ORIGINALS'.split()
 
 
-def filterout(path):
-	path = path.upper()
-	if "\\." in path or "/." in path or "ORIGINALS" in path:
-		return True
-	return False
+def filter_bad(path):
+    return any(s in path.upper() for s in bad_paths)
 
 
-def walk_count_files(_, dirname, names):
-	if filterout(dirname):
-		return
-	for n in names:
-		if n[-4:] in img_extensions:
-			global imgcnt
-			imgcnt += 1
-	sys.stdout.write("\rCounting: %i images." % imgcnt)
+def list_files(dirname):
+    wildcard = dirname.replace('\\', '/')
+    wildcard = wildcard if wildcard.endswith('/') else wildcard+'/'
+    wildcard = wildcard if wildcard.endswith('**') else wildcard+'**'
+    fns = glob(wildcard, recursive=True)
+    fns = [fn for fn in fns if any(fn.endswith(ext) for ext in img_extensions)]
+    return fns
 
 
-def countfiles(sourcedir):
-	imgcnt = 0
-	for root, dirs, files in os.walk(sourcedir):
-		walk_count_files(None, root, files)
-	print()
+def scale_file(targetdir, filename, width, height, imgindex):
+    if filter_bad(filename):
+        return
+    im = Image.open(filename)
+    im = ImageOps.exif_transpose(im)
+    ratio = min(width/float(im.size[0]), height/float(im.size[1]))
+    im = im.resize(tuple(int(x*ratio) for x in im.size), Image.LANCZOS)
+    subdir = os.path.join(targetdir, '%s' % (imgindex//100))
+    if not os.path.exists(subdir):
+        os.mkdir(subdir)
+    outname = '%i.jpg' % imgindex
+    fullname = os.path.join(subdir, outname)
+    im.save(fullname, 'JPEG', quality=98)
 
 
-def walk_scale_files(targetdir, dirname, names):
-	if filterout(dirname):
-		return
-	for n in names:
-		if n[-4:] in img_extensions:
-			global w
-			global h
-			global imgindex
-			global imgcnt
-			sys.stdout.write("\r%s (%.1f %%)                     \n" % (os.path.join(dirname, n), imgindex*100.0/imgcnt))
-			try:
-				im = PIL.Image.open(os.path.join(dirname, n))
-				ratio = min(w/float(im.size[0]), h/float(im.size[1]))
-				im = im.resize(tuple(int(x*ratio) for x in im.size), PIL.Image.ANTIALIAS)
-				subdir = os.path.join(targetdir, "%3.3i" % (imgindex//100))
-				if not os.path.exists(subdir):
-					os.mkdir(subdir)
-				outname = "%i.jpg" % imgindex
-				fullname = os.path.join(subdir, outname)
-				im.save(fullname, "JPEG", quality=98)
-			except Exception as e:
-				print()
-				print(e)
-				sys.exit(1)
-			imgindex += 1
-
-
-def scalefiles(sourcedir, targetdir, width, height):
-	global w
-	global h
-	w = width
-	h = height
-	global imgindex
-	imgindex = 0
-	for root, dirs, files in os.walk(sourcedir):
-		walk_scale_files(targetdir, root, files)
-	print()
+def scalefiles(files, targetdir, width, height):
+    t = tqdm(files)
+    for i,fn in enumerate(t):
+        try:
+            fn = fn.replace('\\', '/')
+            scale_file(targetdir, fn, width, height, i)
+            t.set_postfix(file=fn.rpartition('/')[2])
+        except Exception as ex:
+            print()
+            print(type(ex), ex)
+            break
 
 
 def main():
-	if len(sys.argv) == 5:
-		srcdir = sys.argv[1]
-		tgtdir = sys.argv[2]
-		width = int(sys.argv[3])
-		height = int(sys.argv[4])
-		countfiles(srcdir)
-		try:
-			shutil.rmtree(tgtdir)
-		except:
-			pass
-		try:
-			os.mkdir(tgtdir)
-		except:
-			pass
-		scalefiles(srcdir, tgtdir, width, height)
-	else:
-		print("Usage: %s <src dir> <tgt dir> <width> <height>" % sys.argv[0])
-		print(sys.argv)
-		sys.exit(1)
+    if len(sys.argv) == 5:
+        srcdir = sys.argv[1]
+        tgtdir = sys.argv[2]
+        width = int(sys.argv[3])
+        height = int(sys.argv[4])
+        print('Listing files...')
+        files = list_files(srcdir)
+        print('Found %i images.' % len(files))
+        try:
+            shutil.rmtree(tgtdir)
+        except:
+            pass
+        try:
+            os.mkdir(tgtdir)
+        except:
+            pass
+        scalefiles(files, tgtdir, width, height)
+    else:
+        print('Usage: %s <src dir> <tgt dir> <width> <height>' % sys.argv[0])
+        print(sys.argv)
+        sys.exit(1)
 
 
-if __name__ == "__main__":
-	main()
+if __name__ == '__main__':
+    main()
